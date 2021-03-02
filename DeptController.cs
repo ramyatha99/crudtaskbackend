9bc52ea2247dded30e4bc37b50cfa06dfeaa8ab5
Penumalla Ramyatha <ramyathapenumalla27@gmail.com>
2021-03-02 10:52:25 +0530

    first

---
+++ DeptController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crudtask2.Models;
+
+namespace crudtask2.Controllers
+{
+    public class DeptController : Controller
+    {
+        // GET: Dept
+        public ActionResult Index()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var departmentlist = db.departs.ToList();
+            return View(departmentlist);
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(depart d)
+        {
+            if (ModelState.IsValid)
+            {
+                NORTHWNDEntities db = new NORTHWNDEntities();
+                db.departs.Add(d);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        //for editing
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var data = db.departs.Find(id);
+            return View(data);
+            
+        }
+        [HttpPost]
+        public ActionResult Edit(depart d)
+        {
+            if(ModelState.IsValid)
+            {
+                NORTHWNDEntities db = new NORTHWNDEntities();
+                db.Entry(d).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("index");
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Display(int id)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var data = db.departs.Find(id);
+            return View(data);
+
+        }
+        //[HttpGet]
+        //public ActionResult Delete(int id)
+        //{
+        //    NORTHWNDEntities db = new NORTHWNDEntities();
+        //    var data = db.departs.Find(id);
+        //    return View(data);
+        //}
+        //[HttpPost]
+        public ActionResult Delete(depart d)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var data = db.departs.Find(d.Id);
+           
+            db.departs.Remove(data);
+            
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RegistrationController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crudtask2.Models;
+
+namespace crudtask2.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            ViewBag.DeptId = new SelectList(db.departs, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup( student s)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            if (ModelState.IsValid)
+            {
+                
+                db.students.Add(s);
+                db.SaveChanges();
+               
+               
+            }
+            ViewBag.DeptId = new SelectList(db.departs, "Id", "Name",s.DeptId);
+            return RedirectToAction("Welcome");
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email,string password)
+        {
+           if(ModelState.IsValid)
+            {
+                NORTHWNDEntities db = new NORTHWNDEntities();
+                var data = db.students.Where(m => m.Email == email && m.Password == password).FirstOrDefault();
+                if (data == null)
+                {
+                    ViewBag.finding = "f";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.finding = "g";
+                    return RedirectToAction("Welcome");
+                }
+            }
+            return View();
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Enrolled()
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            ViewBag.DeptId = new SelectList(db.departs, "Id", "Name");
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Enrolled(int? deptid)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            ViewBag.data = db.students.Where(m => m.DeptId == deptid);
+            if (ViewBag.data == null)
+            {
+                ViewBag.dt = "i";
+            }
+             ViewBag.DeptId = new SelectList(db.departs, "Id", "Name");
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+
+    }
+}
